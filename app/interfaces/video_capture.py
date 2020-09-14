f8467210d46f3254c9b712ef389ae41b99d5d390
Igor Simon <igor.simon@timeguilu.com.br>
2020-09-14 13:53:38 -0300

    v0.0.2-IS-feature zoom in and out on ip_stream mode | change portrait/landscape

--- app/interfaces/video_capture.py
+++ app/interfaces/video_capture.py
@@ -7,17 +7,16 @@ class VideoCapture:
         self.window_name = window_name
 
     def screen(self):
-        cv2.namedWindow(self.window_name)
-
-        monitor = {'top': 0, 'left': 0, 'width': 1360, 'height': 768}
+    
+        monitor = {'top': 0, 'left': 0, 'width': 800, 'height': 600}
         sct = mss()
 
         while True:
             img = sct.grab(monitor)
             frame = np.array(img)
-            frame = cv2.resize(frame, tuple([int(measure*0.8) for measure in frame.shape[1::-1]]))
+            frame = cv2.resize(frame, tuple([int(measure*0.3) for measure in frame.shape[1::-1]]))
             # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            cv2.imshow(self.window_name, frame)
+            cv2.imshow("Screen Captures", frame)
             if cv2.waitKey(1) == 27:
                 break
 
@@ -27,18 +26,27 @@ class VideoCapture:
         cap = cv2.VideoCapture(ip_address)
         cv2.namedWindow(self.window_name)
 
+        zoom_mod = 0.3
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
 
-            frame = cv2.resize(frame, tuple([int(measure*0.3) for measure in frame.shape[1::-1]]))
+            frame = cv2.resize(frame, tuple([int(measure*zoom_mod) for measure in frame.shape[1::-1]]))
             if portrait_mode:
                 frame = np.rot90(frame, k=-1, axes=(0, 1))
             # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             cv2.imshow(self.window_name, frame)
-            if cv2.waitKey(1) == 27:
+            
+            key = cv2.waitKey(1)
+            if key == 27:
                 break
+            elif key == 93:
+                zoom_mod += 0.01
+            elif key == 91:
+                zoom_mod -= 0.01
+            elif key == 112:
+                portrait_mode = not portrait_mode
 
         cap.release()
         cv2.destroyAllWindows()
